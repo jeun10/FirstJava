79fa9bc6bd1ff4a471e9d569c6caeccf6c79ba9c
jeun10 <luluka11@naver.com>
2023-01-27 14:07:20 +0900

    First commit

---
+++ HelloTest/src/HelloTest/test1.java
@@ -0,0 +1,20 @@
+package HelloTest;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("정수형 자료형");
+		short b= 500;
+		byte a= 10; //1byte - -128~127
+		int c=1000;
+		long d=6580L;
+		System.out.println("a값:"+a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+
+	}
+
+}
